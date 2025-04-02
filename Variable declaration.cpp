c7cf1a8a6477fde3d6e85e56e9e3ac973adba9da
disha-6303 <contact.disharajput@gmail.com>
2025-04-02 23:42:57 +0530

    Deleted all files

--- Variable declaration.cpp
+++
@@ -1,5 +0,0 @@
-#include <stdio.h>
-int main(){
-	int age=21;
-	printf("I am %d years old",age);
-}
--- floatVarDecl.c
+++
@@ -1,5 +0,0 @@
-#include <stdio.h>
-int main(){
-	float height = 157.49;
-	printf("My height is %f", height);
-}
