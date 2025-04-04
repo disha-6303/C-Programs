a14ec46ac45f6fa41002ccec4bac0bbf061b414e
disha-6303 <contact.disharajput@gmail.com>
2025-04-04 11:09:06 +0530

    Removed all files from GitHub tracking

--- TwoVar.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-int main(){
-	int age = 21;
-	float height= 157.49;
-	
-	printf("I am %d years old and am %.2f cm tall",age, height);
-}
--- varDeclTask.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-int main(){
-	int age= 21;
-	float height= 157.49;
-	float weight= 69.3;
-	
-	printf("I'm %d years old.\nMy height is %.2fcm and I weigh %.2f Kgs.\n",age, height, weight);
-return 0;
-}
