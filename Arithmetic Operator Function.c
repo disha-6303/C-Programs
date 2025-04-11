8823a5160e26d94017b830d93efe56ab79228c23
disha-6303 <contact.disharajput@gmail.com>
2025-04-11 23:39:54 +0530

    💡 Arithmetic Operator Function implementation

---
+++ Arithmetic Operator Function.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+	int A=6, B=3;
+	
+	printf("A + B = %d\n",A + B);
+	printf("A - B = %d\n",A - B);
+	printf("A * B = %d\n",A * B);
+	printf("A / B = %d\n",A / B);
+	printf("A %% B = %d\n",A % B);
+	
+	return 0;
+}
